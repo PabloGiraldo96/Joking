5adcc423ab055a684b71afc2f29708bf03be6055
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2022-10-13 22:36:52 -0500

    Adding files

---
+++ App.js
@@ -0,0 +1,22 @@
+const b = document.querySelector("#movingbutton");
+b.addEventListener("mouseover",change);
+
+const secondButton = document.querySelector('#alert');
+secondButton.addEventListener("mouseover", sonido);
+
+
+function change(){
+	let i = Math.floor(Math.random()*400)+5;
+	let j = Math.floor(Math.random()*300)+1;
+	let z = Math.floor(Math.random()*300)+1;
+	console.log('here' , i ,j , b.style.left , b.style.top);
+    b.style.left = i+'px';
+    b.style.top = j + "px";
+	b.style.bottom = z + "px";
+}
+
+function sonido(){
+const phrase = document.createElement("h2");
+phrase.innerHTML = "JAJAJ Yo sabía mi amor!";
+document.body.appendChild(phrase);
+}
